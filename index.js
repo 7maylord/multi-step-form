386160ae3ef2db8b0e7616a9815199413231a3ea
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-08-31 03:47:19 +0100

    chore:javascript variables

---
+++ index.js
@@ -0,0 +1,12 @@
+// declare variables and store elements
+const sidebarStep = document.querySelectorAll('.indicator-num');
+const formStep = document.querySelectorAll('.step');
+const form = document.getElementById('step-info');
+const planCards = document.querySelectorAll('.plan-card');
+const addonCards = document.querySelectorAll('.addon-card')
+const changePlanBtn = document.getElementById(toggle-plan)
+
+
+
+const nextBtn = document.getElementById('next-button');
+const prevBtn = document.getElementById('prev-button');
